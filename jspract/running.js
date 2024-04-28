4d5aa602559b6fb601364167204ae692ded9219f
fungus2bungus <144967886+fungus2bungus@users.noreply.github.com>
2024-04-28 00:40:18 -0400

    added mission statement template and javascript practice

---
+++ jspract/running.js
@@ -0,0 +1,12 @@
+/*const apples = 5;
+const oranges = 3;
+let total = apples + oranges;
+console.log("total:", total);*/
+
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myApples").textContent = myApples;
+document.getElementById("friendApples").textContent = friendApples;
+document.getElementById("total").textContent = total;
