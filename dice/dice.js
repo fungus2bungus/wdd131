52242b3f418a71a66f6518d7e4a297e4019c01d8
fungus2bungus <cmad29@protonmail.com>
2024-10-23 18:32:35 -0600

    updated js and html for dice, added placehoder imgs

--- dice/dice.js
+++ dice/dice.js
@@ -8,15 +8,80 @@ function randRange(min, max) {
 
 
 let g = 4;
-dicemount = [];
+let dicemount = [];
+let resarry = [];
 
-for (let i = 0; i <= g; i++) {
-    dicemount.push(randRange(1,6));
+// for (let i = 0; i < g; i++) {
+//     dicemount.push(randRange(1,6));
+// }
+
+// for (let i = 0; i < dicemount.length; i++) {
+//     console.log(dicemount[i]);
+// }
+
+function addRollResult(entry) {
+    return `<p>${entry.id}: ${entry.result}</p>`
 }
 
-for (let i = 0; i < dicemount.length; i++) {
-    console.log(dicemount[i]);
+
+const dires = document.querySelector("#disresults");
+
+
+function roll(event) {
+    const dnum = event.target;
+    //console.log(dnum.id);
+/*     if (dnum.id == "d4") {
+        dicemount.push(randRange(1,6));
+        console.log(dicemount);
+        document.querySelector("#disresult").textContent = dicemount;
+    } */
+   const res = document.querySelector("#results");
+// const newhtml = dicemount.map(addRollResult);
+
+    switch(event.target.id) {
+        case "d4":
+            //dicemount.push(randRange(1,4))
+            //document.querySelector("#disresult").textContent = dicemount + " ";
+            //console.log(newhtml);
+            result = randRange(1,4);
+            //dires.innerHTML = dicemount.map(addRollResult).join("");
+            break;
+        case "d6":
+            //dicemount.push(randRange(1,6))
+            //document.querySelector("#disresult").textContent = dicemount + " ";
+            //console.log(newhtml);
+            //dires.innerHTML = dicemount.map(addRollResult).join(""); 
+            result = randRange(1,6);
+            break;
+        case "d8":
+            result = randRange(1,8);
+            break;
+        case "d10":
+            result = randRange(1,10);
+            break;
+        case "d12":
+            result = randRange(1,12);
+            break;
+        case "d20":
+            result = randRange(1,20);
+            break;
+        case "d100":
+            result = randRange(1,100);
+            break;
+        case "quickRoll":
+            return;
+    }
+
+    resarry.push({id: dnum.id, result });
+    dicemount.push(result);
+    dires.innerHTML = resarry.map(addRollResult).join("");
+    // console.log(dicemount);
 }
 
 //console.log(randRange(1,6));
-console.log(dicemount);
+
+
+//localStorage.setItem("name", myName);
+
+document.querySelector("#quickRoll").addEventListener("click", roll);
+// console.log(dicemount); 
