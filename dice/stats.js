af38e0e247c16f7e81c79d9bf692c65f9c7a00d5
fungus2bungus <cmad29@protonmail.com>
2024-10-30 09:11:00 -0600

    mode function now works, might need to adjust

--- dice/stats.js
+++ dice/stats.js
@@ -11,6 +11,7 @@ const dlen = rlist.length;
 let total = 0;
 let avg = 0;
 let highRoll = 0;
+let mo = [];
 /* for(let i = 0; i< rlist.length; i++) {
     total += rlist[i]; 
 } */
@@ -19,6 +20,7 @@ function doCalc(arr) {
     total = 0;
     avg = 0;
     highRoll = 0;
+    mo = [];
     const jlist = JSON.parse(arr);
     for(let i = 0; i<jlist.length; i++) {
         total+=jlist[i];
@@ -29,8 +31,12 @@ function doCalc(arr) {
     // console.log(mgd);
     console.log("Highest Roll",highRoll);
     console.log("Length: ", jlist.length);
+    console.log("list: ", jlist);
     // document.querySelctor()
     // document.querySelector(hgs[]).textContent=avg.toFixed(2);
+    mo = findMode(jlist);
+    // console.log(findMode);
+
 
 
 }
@@ -40,21 +46,45 @@ hsd = ["d4","d6","d8","d10","d12","d20","d100"]
 hws = [localStorage.d4list, localStorage.d6list, localStorage.d8list, localStorage.d10list, localStorage.d12list, localStorage.d20list, localStorage.d100list];
 //console.log("Test of local storage value: ", localStorage.(hsd[0]+"list");
 
-findMode(arr) {
+function findMode(list) {
     const freqmap = {};
-    arr.forEach(item => {
+    list.forEach(item => {
         freqmap[item] = (freqmap[item] || 0)+1;
-    });
+    }); 
+
+    let maxFreq = 0;
+    for (const item in freqmap) {
+        if (freqmap[item] > maxFreq) {
+            maxFreq = freqmap[item];
+        }
+    }
+    const modes = [];
+    for (const item in freqmap) {
+        if (freqmap[item] === maxFreq) {
+            modes.push(item);
+        }
+    }
     
 
+    return modes;
+
 }
+
+
+  
+  // Example usage
+//   const myList = [1, 2, 3, 2, 4, 2, 5, 1];
+//   console.log(findMode(myList)); // Output: ['2']
+
 
 for(let k = 0; k<7; k++) {
     // console.log(localStorage.hgs[k]);
     doCalc(hws[k]);
+    // findModes(hws[k]);
     document.querySelector("#"+hsd[k]+"avg").textContent=(hsd[k]+": "+avg.toFixed(2));
     document.querySelector("#"+hsd[k]+"total").textContent=(hsd[k]+": "+total);
     document.querySelector("#"+hsd[k]+"max").textContent=(hsd[k]+": "+highRoll);
+    document.querySelector("#"+hsd[k]+"mode").textContent=(hsd[k]+": "+mo);
 
 }
 
